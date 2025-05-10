965ab94a190ee7e0798efa072720c1b07853c87c
saintkedro <isongkingsley101@gmail.com>
2025-05-10 18:14:11 +0100

    updated variables from underscore case to camel case

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        int percentage = Console.ReadLine
     }
 }
