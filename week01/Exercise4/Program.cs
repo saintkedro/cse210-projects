a7c3ca221ddc427c697fc8579e6c15fd971dad7b
saintkedro <isongkingsley101@gmail.com>
2025-05-13 00:39:55 +0100

    completed the first part of Exercise 4 - List

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 to quit. ");
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+
+        {
+            Console.Write("Enter a number: ");
+
+            string userResponse = Console.ReadLine();
+
+            userNumber = int.Parse(userResponse);
+
+            numbers.Add(userNumber);
+        }
+
+        // Calculating the sum
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of the numbers are: {sum}");
+        
+    
+
+
+            
+        
+
     }
 }
